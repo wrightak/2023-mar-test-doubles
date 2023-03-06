5376a9c7aa89d6c80d58dcab29cedfe9b0d067e8
Andrew Wright <wrightak@gmail.com>
2023-03-06 12:56:45 +0900

    Initial commit

---
+++ src/main/java/missile/Launcher.java
@@ -0,0 +1,6 @@
+package missile;
+
+public class Launcher {
+    static void launchMissile(Missile missile, LaunchCode launchCode) {
+    }
+}
---
+++ src/test/java/missile/LauncherTest.java
@@ -0,0 +1,4 @@
+package missile;
+
+class LauncherTest {
+}
